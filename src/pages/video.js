e8313ac6252ca2ac63b4dc6e63c85634f852fc52
DevNicolasLacerda <nicklacerda2018@gmail.com>
2023-06-15 00:51:55 -0300

    video layout criado

---
+++ src/pages/video.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react'
+import './video.css'
+
+function Video() {
+
+  const videoRef = useRef(null)
+  const [ play, setPlay ] = useState(false)
+
+  function handleStart() {
+    if (play) {
+      videoRef.current.pause()
+      setPlay(false)
+    } else {
+      videoRef.current.play()
+      setPlay(true)
+    }
+  }
+
+  return (
+    <div className='video'>
+      <video 
+          className='videoPlayer'
+          ref={videoRef}
+          onClick={handleStart}
+          loop
+          src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+      ></video>
+    </div>
+  )
+}
+
+export default Video
